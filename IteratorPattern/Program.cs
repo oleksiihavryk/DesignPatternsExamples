44bb6647feda734bed848d7276df1368f1d36f9e
oleksiihavryk <oleksii.havryk2004@gmail.com>
2023-09-06 20:23:11 +0300

    Add "Iterator" pattern.

---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Iterator pattern also build-in for C# language.
+ * Almost all collection in .NET framework class library have interface IEnumerable
+ * such as List<T>, Stack<T>, Queue<T>, Dictionary<TKEy, TValue> and ect.
+ */
+var list = new List<int> { 1, 2, 3, 100 };
+
+//There is IEnumerator interface is Iterator pattern.
+Console.WriteLine("Build-in FCL implementation of Iterator pattern.");
+var enumerator = list.GetEnumerator();
+try
+{
+    int num;
+    while (enumerator.MoveNext())
+    {
+        num = enumerator.Current;
+        Console.WriteLine(num);
+    } 
+}
+finally
+{
+    enumerator.Dispose();
+}
+//or just use build-in foreach loop (same code when compiled as previous)
+Console.WriteLine("foreach loop.");
+foreach (var item in list)
+    Console.WriteLine(item);
+
